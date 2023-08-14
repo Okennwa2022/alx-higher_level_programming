972d867582503ff45fd6ee46e7737eec3339f205
Okennwa2022 <adindumichael@yahoo.com>
2023-08-14 14:53:36 -0700

    first commit

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if rev_list = my_list[::1]
+    for o in range (len(rev_list)):
+        print('{:d}'. format(rev_list[o]))
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for o in my_list:
+            if o > max:
+                max = o
+                return (max)
+            else:
+                return(none)

333fe66398c25bf28034021c6decfe882a013b9b
Okennwa2022 <adindumichael@yahoo.com>
2023-08-28 21:51:24 -0700

    first commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+sum = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (sum)
